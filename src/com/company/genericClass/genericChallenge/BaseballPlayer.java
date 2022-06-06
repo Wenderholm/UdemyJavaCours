4c44f0288d642a556a83d5780255d436f73248c2
wenderholm <pwenderholm@gmail.com>
2022-06-06 16:56:25 +0200

    genericChallenge finish

---
+++ src/com/company/genericClass/genericChallenge/BaseballPlayer.java
@@ -0,0 +1,9 @@
+package com.company.genericClass.genericChallenge;
+/**
+ * Created by dev on 17/10/2015.
+ */
+public class BaseballPlayer extends Player {
+    public BaseballPlayer(String name) {
+        super(name);
+    }
+}
