0b30c83121336e1116904859fc8b83714ff781ae
pwenderholm <19Wend!88>
2022-06-30 15:10:31 +0200

    StockItem map exercise cz I

--- src/com/company/genericClass/genericChallenge/Main.java
+++ src/com/company/genericClass/genericChallenge/Main.java
@@ -1,8 +1,21 @@
 package com.company.genericClass.genericChallenge;
 
-public class Main {
 
+public class Main {
     public static void main(String[] args) {
+        // ArrayList<Team> teams;
+        // Collections.sort(teams);
+        // Create a generic class to implement a league table for a sport.
+        // The class should allow teams to be added to the list, and store
+        // a list of teams that belong to the league.
+        //
+        // Your class should have a method to print out the teams in order,
+        // with the team at the top of the league printed first.
+        //
+        // Only teams of the same type should be added to any particular
+        // instance of the league class - the program should fail to compile
+        // if an attempt is made to add an incompatible team.
+
 
         League<Team<FootballPlayer>> footballLeague = new League<>("AFL");
         Team<FootballPlayer> adelaideCrows = new Team<>("Adelaide Crows");
@@ -22,45 +35,7 @@ public class Main {
         footballLeague.add(fremantle);
 
 //        footballLeague.add(baseballTeam);
-        footballLeague.showLeagueTable();
-
-        BaseballPlayer pat = new BaseballPlayer("Pat");
-        SoccerPlayer beckham = new SoccerPlayer("Beckham");
-        Team rawTeam = new Team("Raw Team");
-        rawTeam.addPlayer(beckham); // unchecked warning
-        rawTeam.addPlayer(pat);     // unchecked warning
-
-        footballLeague.add(rawTeam);     // unchecked warning
-
-        League<Team> rawLeague = new League<>("Raw");
-        rawLeague.add(adelaideCrows);     // no warning
-        rawLeague.add(baseballTeam);    // no warning
-        rawLeague.add(rawTeam);         // no warning
-
-        League reallyRaw = new League("Really raw");
-        reallyRaw.add(adelaideCrows);     // unchecked warning
-        reallyRaw.add(baseballTeam);    // unchecked warning
-        reallyRaw.add(rawTeam);         // unchecked warning
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//        footballLeague.showLeague();
 
     }
 }
---
+++ src/com/company/javaCollection/BasketCollection/Main.java
@@ -0,0 +1,4 @@
+package com.company.javaCollection.BasketCollection;
+
+public class Main {
+}
