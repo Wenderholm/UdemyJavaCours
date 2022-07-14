4d4a540ed1233fc3242b7ef99d0262f01874087f
pwenderholm <19Wend!88>
2022-07-14 14:04:24 +0200

    exception exercise

--- src/com/company/exceptions/Example.java
+++ src/com/company/exceptions/Example.java
@@ -1,5 +1,6 @@
 package com.company.exceptions;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Example {
@@ -12,7 +13,6 @@ public class Example {
     private static int divide(){
         int x = getInt();
         int y = getInt();
-
         System.out.println("x is " + x + ", y is " + y) ;
         return x / y;
     }
@@ -20,6 +20,14 @@ public class Example {
     private static int getInt(){
         Scanner s = new Scanner(System.in);
         System.out.println("Please enter an integer ");
-        return s.nextInt();
+        while(true){
+            try{
+                return s.nextInt();
+            }catch(InputMismatchException e){
+                s.nextLine();
+                System.out.println("Please enter a number using only the digits 0 to 9");
+            }
+        }
     }
+
 }
